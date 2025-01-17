9b907d4ed8e77f23468d54b35fcf840b9a3642f7
Mohsen Azimi <me@azimi.me>
2025-01-17 11:19:46 +0700

    test: add npm update notifier test

--- tests/node_e2e_test.rs
+++ tests/node_e2e_test.rs
@@ -1,4 +1,5 @@
 use assert_cmd::Command;
+use predicates::prelude::*;
 use std::error::Error;
 use std::path::PathBuf;
 
@@ -31,6 +32,62 @@ fn test_nodejs_ecosystem_ignore_tools() -> Result<(), Box<dyn Error>> {
         .arg("yarn,pnpm");
 
     cmd.assert().success();
+
+    Ok(())
+}
+
+#[test]
+fn test_npm_outdated_nag() -> Result<(), Box<dyn Error>> {
+    // Use the pre-existing nagging package
+    let nagging_package = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("tests")
+        .join("test_files")
+        .join("nagging-package");
+
+    // First, ensure we're starting with a clean environment by unsetting any existing env vars
+    let mut reset_cmd = Command::new("npm");
+    reset_cmd
+        .env_remove("NPM_CONFIG_UPDATE_NOTIFIER")
+        .args(["config", "delete", "update-notifier"])
+        .current_dir(&nagging_package)
+        .assert()
+        .success();
+
+    // Set update-notifier to true to ensure we start in a nagging state
+    let mut enable_cmd = Command::new("npm");
+    enable_cmd
+        .env_remove("NPM_CONFIG_UPDATE_NOTIFIER")
+        .args(["config", "set", "update-notifier", "true"])
+        .current_dir(&nagging_package)
+        .assert()
+        .success();
+
+    // Verify update-notifier is enabled
+    let mut verify_cmd = Command::new("npm");
+    verify_cmd
+        .env_remove("NPM_CONFIG_UPDATE_NOTIFIER")
+        .args(["config", "get", "update-notifier"])
+        .current_dir(&nagging_package)
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("true"));
+
+    // Run stop-nagging to set environment vars that silence the notices
+    let mut stop_nagging_cmd = Command::cargo_bin("stop-nagging")?;
+    stop_nagging_cmd
+        .arg("--ecosystems")
+        .arg("nodejs")
+        .assert()
+        .success();
+
+    // Check npm config after running stop-nagging
+    let mut post_config_cmd = Command::new("npm");
+    post_config_cmd
+        .args(["config", "get", "update-notifier"])
+        .current_dir(&nagging_package)
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("false"));
 
     Ok(())
 }
